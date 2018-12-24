8bac57c76abde0ebcc28e94f9adf5e974d772e67
Semih Orhan <semih99orhan@gmail.com>
2018-12-24 18:57:36 +0100

    Added an extra check to keep track of the wall state

--- Assets/_Scripts/Cell.cs
+++ Assets/_Scripts/Cell.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    // This will keep a reference to the walls that have been set in the editor
-    [SerializeField] private GameObject[] walls;
-
     // This enum is here so removing walls can be done from other scripts
     // without confusion on which wall is being removed
     public enum CellWalls
@@ -17,23 +14,60 @@ public class Cell : MonoBehaviour
         LeftWall
     }
 
+    // This will keep a reference to the walls that have been set in the editor
+    [SerializeField] private GameObject[] walls;
+    private bool[] wallState = new bool[] { true, true, true, true };
+
     // Removes a wall with the help of the enum
     public void RemoveWall(CellWalls wall)
     {
         switch (wall)
         {
             case CellWalls.TopWall:
-                walls[0].SetActive(false);
+                if (GetWallState(wall))
+                {
+                    walls[0].SetActive(false);
+                    wallState[0] = false;
+                }
                 break;
             case CellWalls.RightWall:
-                walls[1].SetActive(false);
+                if (GetWallState(wall))
+                {
+                    walls[1].SetActive(false);
+                    wallState[1] = false;
+                }
                 break;
             case CellWalls.BottomWall:
-                walls[2].SetActive(false);
+                if (GetWallState(wall))
+                {
+                    walls[2].SetActive(false);
+                    wallState[2] = false;
+                }
                 break;
             case CellWalls.LeftWall:
-                walls[3].SetActive(false);
+                if (GetWallState(wall))
+                {
+                    walls[3].SetActive(false);
+                    wallState[3] = false;
+                }
                 break;
         }
+    }
+
+    public bool GetWallState(CellWalls wall)
+    {
+        switch (wall)
+        {
+            case CellWalls.TopWall:
+                return wallState[0];
+            case CellWalls.RightWall:
+                return wallState[1];
+            case CellWalls.BottomWall:
+                return wallState[2];
+            case CellWalls.LeftWall:
+                return wallState[3];
+        }
+
+        return false;
     }
 }
