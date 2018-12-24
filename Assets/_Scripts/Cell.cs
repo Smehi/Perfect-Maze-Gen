d39c09e740bd97645f888d50fb9771c7273379f6
Semih Orhan <semih99orhan@gmail.com>
2018-12-24 18:33:53 +0100

    Got the start of the maze working.
    Added a Cell script that contains all the walls and a method to remove the wall with the help of an enum.
    Added a MazeManager script that will make a grid of cells with the size calculated from the screen and maze dimensions.

---
+++ Assets/_Scripts/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    // This will keep a reference to the walls that have been set in the editor
+    [SerializeField] private GameObject[] walls;
+
+    // This enum is here so removing walls can be done from other scripts
+    // without confusion on which wall is being removed
+    public enum CellWalls
+    {
+        TopWall,
+        RightWall,
+        BottomWall,
+        LeftWall
+    }
+
+    // Removes a wall with the help of the enum
+    public void RemoveWall(CellWalls wall)
+    {
+        switch (wall)
+        {
+            case CellWalls.TopWall:
+                walls[0].SetActive(false);
+                break;
+            case CellWalls.RightWall:
+                walls[1].SetActive(false);
+                break;
+            case CellWalls.BottomWall:
+                walls[2].SetActive(false);
+                break;
+            case CellWalls.LeftWall:
+                walls[3].SetActive(false);
+                break;
+        }
+    }
+}
---
+++ Assets/_Scripts/MazeManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour
+{
+    [Header("Maze properties")]
+    [SerializeField] private int mazeRows;
+    [SerializeField] private int mazeColumns;
+    [SerializeField] private GameObject mazeCanvas;
+
+    [Header("Cell properties")]
+    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] private Dictionary<GameObject, Cell> mazeCells = new Dictionary<GameObject, Cell>();
+    private float cellWidth;
+    private float cellHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        InitVars();
+        CreateGrid();
+    }
+
+    private void InitVars()
+    {
+        // Remove all the cells we currently have
+        for (int i = 0; i < mazeCanvas.transform.childCount; i++)
+        {
+            Destroy(mazeCanvas.transform.GetChild(i));
+        }
+
+        // Recalculate the cell width and height
+        cellWidth = Screen.width / mazeColumns;
+        cellHeight = Screen.height / mazeRows;
+
+        // Empty the cells list for repopulation
+        mazeCells.Clear();
+    }
+
+    private void CreateGrid()
+    {
+        Vector2 spawnPos = Vector2.zero;
+
+        // Nested for-loop to fill the screen with cells
+        for (int y = 0; y < mazeRows; y++)
+        {
+            for (int x = 0; x < mazeColumns; x++)
+            {
+                // Make the cell and set the properties
+                GameObject cell = Instantiate(cellPrefab);
+
+                // We set the parent first because we want to set the position relative to the canvas
+                cell.transform.SetParent(mazeCanvas.transform);
+
+                // Set the position with a small offset because the pivot point of the cell is in the center
+                cell.transform.position = new Vector2(x * cellWidth + (cellWidth / 2), y * cellHeight + (cellHeight / 2));
+                cell.transform.localScale = new Vector2(cellWidth, cellHeight);
+
+                mazeCells.Add(cell, cell.GetComponent<Cell>());
+            }
+        }
+    }
+}
